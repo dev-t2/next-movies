1fe39cff5e759075717956ebcc220031d6ea481b
t2 <oo_ri@naver.com>
2021-04-19 04:21:50 +0900

    Styled Components Install and App.tsx Style Update

--- App.tsx
+++ App.tsx
@@ -1,6 +1,24 @@
 import React, { memo, useEffect, useState } from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import { Camera } from 'expo-camera';
+import styled from 'styled-components/native';
+
+const StyledView = styled.SafeAreaView({
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#0984e3',
+});
+
+const StyledText = styled.Text({
+  color: '#fff',
+  fontSize: 24,
+});
+
+const StyledCamera = styled(Camera)({
+  width: '100%',
+  height: '100%',
+});
 
 const App = () => {
   const [hasPermission, setHasPermission] = useState<null | boolean>(null);
@@ -14,21 +32,25 @@ const App = () => {
   }, []);
 
   if (hasPermission === null) {
-    return <ActivityIndicator />;
+    return (
+      <StyledView>
+        <ActivityIndicator />
+      </StyledView>
+    );
   }
 
   if (hasPermission === false) {
     return (
-      <View>
-        <Text>No access to camera</Text>
-      </View>
+      <StyledView>
+        <StyledText>No access to camera</StyledText>
+      </StyledView>
     );
   }
 
   return (
-    <View>
-      <Text>Access to camera</Text>
-    </View>
+    <StyledView>
+      <StyledCamera type={Camera.Constants.Type.front} />
+    </StyledView>
   );
 };
 
