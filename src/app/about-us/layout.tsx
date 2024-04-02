b8950e4616f8a9eacc2bb50e5739b5f98a5f1d1c
dev-t2 <oo_ri@naver.com>
2024-04-03 00:09:57 +0900

    Metadata Update

--- src/app/about-us/layout.tsx
+++ src/app/about-us/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About Us',
+};
+
 export default function AboutUsLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div>
